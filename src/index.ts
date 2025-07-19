c6c8b43769b8ceb5f7438a58592d152e62b17ca5
Leon <leon.buntis@gmail.com>
2025-07-19 23:39:35 +0300

    Add express server setup for web integration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import { telegramBotToken } from './config';
 import { YeaBuddyBot } from './bot/YeaBuddyBot';
+import express from "express";
+
+const port = process.env.PORT || 4000;
 
 const bot = new YeaBuddyBot(telegramBotToken);
-bot.run().catch(console.error);
+// bot.run().catch(console.error);
+
+const app = express();
+
+app.use(async () => await bot.runWeb("yeabuddybot.onrender.com"));
+
+// app.get('/', (req, res) => {
+//     res.send('YeaBuddy Bot Web Interface');
+// });
+
+app.listen(port, () => console.log("Listening on port", port));
