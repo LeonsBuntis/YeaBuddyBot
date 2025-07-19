51e3c154c0346db59b834ca85ffa5b1298eae369
Leon <leon.buntis@gmail.com>
2025-07-19 22:09:24 +0300

    remove webserver

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { telegramBotToken } from './config';
 import { YeaBuddyBot } from './bot/YeaBuddyBot';
-import { WebServer } from './web/webserver';
 
 const bot = new YeaBuddyBot(telegramBotToken);
 bot.run().catch(console.error);
-
-const webServer = new WebServer(undefined);
-webServer.run().catch(console.error);
